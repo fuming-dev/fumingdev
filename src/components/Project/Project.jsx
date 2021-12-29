ae38b5256797cac9e398c067be6f15061e798101
fuming-dev <fhuangasuna@gmail.com>
2021-12-28 21:09:45 -0600

    more changes

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,5 +1,6 @@
 import "./project.css";
 import { projects } from "../../data";
+import { FaGithub } from "react-icons/fa";
 
 const Project = () => {
   return (
@@ -9,17 +10,16 @@ const Project = () => {
       <section className="project-container">
         {projects.map((project) => {
           return (
-            <div className="section" >
+            <div className="section">
               <p>Title: {project.title}</p>
               <p>Language(s): {project.subtitle}</p>
               <img src={project.image} alt="Facebook Clone" />
               <div className="section-content">
                 <a href={project.github}>
-                    <i class="fab fa-github"></i>
+                  <FaGithub size={32} />
                 </a>
                 <p>Description: {project.description}</p>
               </div>
-
             </div>
           );
         })}
