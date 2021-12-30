20ec9773f2a98db8a58dc4dd5071b83a6670e372
fuming-dev <fhuangasuna@gmail.com>
2021-12-30 17:29:54 -0600

    test

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+
+import { FiMail } from "react-icons/fi";
 
 const Contact = () => {
-    return (
-        <div className='contact-container' id='Contact'>
-            <h1>Contact Me</h1>
-            <div className="border-top"></div>
-            <p>Currently available for work</p>
-            <button ><a href = "mailto: fhuangasuna@gmail.com"><i class="fas fa-envelope-square"></i> Send Email</a></button>
+  return (
+    <div className="contact-container" id="Contact">
+      <h1>Contact Me</h1>
+      <div className="border-top"></div>
+      <p>Currently available for work</p>
+      <div className="btn">
+        <button>
+          <FiMail size={24} />
+          <a href="mailto: fuming.dev@gmail.com">Email Here</a>
+        </button>
 
+      </div>
+      
+    </div>
 
-        </div>
-    )
-}
+  );
+};
 
-export default Contact
+export default Contact;
